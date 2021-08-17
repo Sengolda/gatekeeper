0cbe646e61665c4cbf0c2411af130c1680ddf6b5
Sengolda <senushkabatawala@gmail.com>
2021-08-17 20:28:15 +0530

    commits

---
+++ bot.py
@@ -0,0 +1,17 @@
+import discord
+from discord.ext import commands
+
+
+class GateKeepBot(commands.Bot):
+	def __init__(self):
+		super().__init__(command_prefix="g.", intents=discord.Intents.all())
+
+
+	async def on_ready(self):
+		print(f'Logged in as {self.user} (ID: {self.user.id})')
+		print('-'*52)
+
+
+bot = GateKeepBot()
+bot.load_extension('cogs.gatekeep')
+bot.run('ODA1MzI2NTEwNjEyNDE0NTA0.YBZQwg.rc8PvgbqCNOnMozxAyw4ujUukmQ')
