bff7691243640e9f221251509f4da387aa8d95f3
Lint Action <lint-action@samuelmeuli.com>
2021-09-16 15:03:42 +0000

    Fix code style issues with Black

--- bot.py
+++ bot.py
@@ -3,15 +3,14 @@ from discord.ext import commands
 
 
 class GateKeepBot(commands.Bot):
-	def __init__(self):
-		super().__init__(command_prefix="g.", intents=discord.Intents.all())
+    def __init__(self):
+        super().__init__(command_prefix="g.", intents=discord.Intents.all())
 
-
-	async def on_ready(self):
-		print(f'Logged in as {self.user} (ID: {self.user.id})')
-		print('-'*52)
+    async def on_ready(self):
+        print(f"Logged in as {self.user} (ID: {self.user.id})")
+        print("-" * 52)
 
 
 bot = GateKeepBot()
-bot.load_extension('cogs.gatekeep')
-bot.run('')
+bot.load_extension("cogs.gatekeep")
+bot.run("")
